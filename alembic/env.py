dfeb0bd3b80faa782548e54b6b113837f0775d97
mohamadreza1992 <khoshniatmohamadreza@gmail.com>
2026-07-22 15:59:00 +0330

    feat(auth): add user model and migration

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 from app.core.config import settings
 from app.database.base import Base
+from app.features.auth import models  # noqa: F401
 
 config = context.config
 
