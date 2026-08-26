4407a3300a84f71ff5f1be2b5d55106dd8bf6e3e
mohamadreza1992 <khoshniatmohamadreza@gmail.com>
2026-08-26 12:25:53 +0330

    refactor: configure application logging

--- app/main.py
+++ app/main.py
@@ -2,10 +2,15 @@ from fastapi import FastAPI
 
 from app.api.router import api_router
 from app.core.config import settings
+from app.core.logging import setup_logging
+
+setup_logging()
+
 
 app = FastAPI(
     title=settings.app_name,
     version=settings.app_version,
 )
+
 
 app.include_router(api_router)
